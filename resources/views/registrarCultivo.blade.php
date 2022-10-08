7141f8006abfa40a5662c3c990ccfc69f8369025
Unkn0wn-7 <GM21007@ues.edu.sv>
2022-10-08 17:58:45 -0600

    Registrar cultivo
    Se creo la vista para registrar los cultivos

---
+++ resources/views/registrarCultivo.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar Nuevo Cultivo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class = "container">
+        <h4>Registrar nuevo cultivo</h4>
+        <br>
+        <div class ="row">
+            <div class="col-x1-12">
+                <form action="{{route('')}}"method="post">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" name="nombre" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="temperaturaMax">Temperatura Maxima</label>
+                        <input type="number" class="form-control" name="temperaturaMax" required min="0" step="0.1">
+                    </div>
+                    <div class="form-group">
+                        <label for="temperaturaMin">Temperatura Minima</label>
+                        <input type="number" class="form-control" name="temperaturaMin" required min="0" step="0.1">
+                    </div>
+                    <div class="form-group">
+                        <label for="humedadMax">Humedad Maxima</label>
+                        <input type="number" class="form-control" name="humedadMax" required min="0" step="0.1">
+                    </div>
+                    <div class="form-group">
+                        <label for="humedadMin">Humedad Minima</label>
+                        <input type="number" class="form-control" name="humedadMin" required min="0" step="0.1">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Registrar">
+                        <input type="reset" class="btn btn-default" value="Cancelar">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
