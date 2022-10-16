40406d99604763ebb375131474e19f1ac47dfafc
isaac <ra20056@ues.edu.sv>
2022-10-16 09:58:02 -0600

    Recreacion del Modelo Cultivo con migracion y creacion del controlador de cultivo
    Hola :)

---
+++ app/Models/Cultivo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cultivo extends Model
+{
+    use HasFactory;
+}
