493a9ee958863f76e6662a1bb51a5e698bbcc4c9
Santiago Tadeo Del Pinal <dp19004@ues.edu.sv>
2022-10-25 16:44:29 -0600

    Alertas en el Dashboard
    No esta perfecto pero algo es algo. falta pulirlo

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,12 @@ Route::resource('alarmas', 'App\Http\Controllers\AlarmaController');
 Route::resource('registros', 'App\Http\Controllers\RegistroController');
 Route::resource('Zonas','App\Http\Controllers\ZonaController');
 Route::resource('cultivos','App\Http\Controllers\CultivoController');
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::resource('dashboard', 'App\Http\Controllers\NivelController');
 });
