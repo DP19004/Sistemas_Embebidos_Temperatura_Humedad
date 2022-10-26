4ea9fb745c74688f301563d8dd4b59eb7bf677de
KarenSSanchezP <sp21013@ues.edu.sv>
2022-10-26 00:06:20 -0600

    Se oculto la vista para registrarse en welcome.blade.php, si utilizan el comando 'php artisan migrate:refresh' pueden habilitar temporalmente la vista registrar para poder ingresar al dashboard, de lo contrario basta con el login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,9 +29,10 @@
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
+                    <!-- Comentando esta parte, al usuario ya no le aparece la opcion de registrarse  
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a> 
+                    @endif -->
                     @endauth
                 </div>
             @endif
