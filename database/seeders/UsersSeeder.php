d6564360c80c8ab27909424267f4a1f0bc432211
KarenSSanchezP <sp21013@ues.edu.sv>
2022-10-27 17:44:50 -0600

    Se creo el seeder de users, ya pueden ingresar al proyecto solo usando el login

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        
+        $user->name = "Admin";
+        $user->email = "admin@gmail.com";
+        $user->password = bcrypt('admin1234');
+
+        $user->save();
+    }
+}
