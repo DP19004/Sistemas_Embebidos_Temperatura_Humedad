e341f6a7669376f4c8c1a1e2198bd71c3fc2d1f7
Martin Yanes <yd18001@ues.edu.sv>
2022-11-07 20:53:14 -0600

    Form-Edit in Card
    Editar Cultivo y KitSensor en card, pendiente de los form alarma y zonas, los tengo en proceso

--- resources/views/Cultivo/editarCultivo.blade.php
+++ resources/views/Cultivo/editarCultivo.blade.php
@@ -3,24 +3,15 @@
 @section('title', 'Editar | Cultivo')
 @section('content')
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Cultivo</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <div class = "container">
-        <h4 class="heading" style="text-align: center;">Editar cultivo</h4>
-        <br>
-        <div class ="d-flex align-items-center justify-content-center">
-            <div class="col-md-4 border">
+<!-- Card -->
+<div class="d-flex">
+    <div class="card mx-auto" style="width: 60%; margin-top: 2%;">
+        <h5 class="card-header text-center text-white bg-dark" >Editar Cultivo</h5>
+            <div class="card-body">
+                <!-- Formulario -->
                 <form action="/cultivos/{{$cultivo->id}}"method="POST">
-                   @csrf
-                   @method('PUT')
+                    @csrf
+                    @method('PUT')
                     <div class="form-group">
                         <label for="nombre">Nombre</label>
                         <input type="text" class="form-control" name="nombre" required value="{{$cultivo->nombre}}">
@@ -41,14 +32,16 @@
                         <label for="humedadMin">Humedad Minima</label>
                         <input type="number" class="form-control" name="humedadMin" required min="0" max="1" step="0.01" value="{{$cultivo->HumedadMinima}}">
                     </div>
+                    <!-- Botones -->
                     <div class="form-group">
                         <input type="submit" class="btn btn-primary" value="Registrar">
                         <a href="/cultivos" class="btn btn-secondary">Cancelar</a>
                     </div>
                 </form>
+                <!-- /Formulario -->
             </div>
-        </div>
     </div>
-</body>
-</html>
+</div>
+<!-- /Card -->
+
 @endsection
--- resources/views/kitSensor/edit.blade.php
+++ resources/views/kitSensor/edit.blade.php
@@ -3,28 +3,35 @@
 @section('title', 'Kit de Sensores|Editar')
 @section('content')
 
-<div class="mx-auto" style="width:600px;">
-<br>
-<h2 class="display-5">Editar Kit Sensor</h2>
-<form action="/kitSensores/{{$kitSensor->id}}" method="POST">
-    @csrf    
-    @method('PUT')
-    <div class="mb-3">
-        <label for="" class="form-label">Serial de Humedad</label>
-        <input id="serialHumedad" name="serialHumedad" type="text"  class="form-control" value="{{$kitSensor->serialHumedad}}" pattern="[0-9]+">
+<!-- Card -->
+<div class="d-flex">
+    <div class="card mx-auto" style="width: 60%; margin-top: 2%;">
+        <h5 class="card-header text-center text-white bg-dark" >Editar Cultivo</h5>
+            <div class="card-body">
+                <!-- Formulario -->
+                <form action="/kitSensores/{{$kitSensor->id}}" method="POST">
+                    @csrf    
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="" class="form-label">Serial de Humedad</label>
+                        <input id="serialHumedad" name="serialHumedad" type="text"  class="form-control" value="{{$kitSensor->serialHumedad}}" pattern="[0-9]+">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Serial de Temperatura</label>
+                        <input id="serialTemperatura" name="serialTemperatura" type="text"  class="form-control" value="{{$kitSensor->serialTemperatura}}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Fecha de Colocacion</label>
+                        <input id="fechaColocacion" name="fechaColocacion" type="date"  class="form-control" value="{{$kitSensor->fechaColocacion}}">
+                    </div>
+                    
+                    <!--botones-->
+                    <a href="/kitSensores" class="btn btn-secondary" tabindex="8">Cancelar</a>
+                    <button type="submit" class="btn btn-primary" tabindex="9">Guardar</button>
+                </form>
+            </div>
     </div>
-    <div class="mb-3">
-        <label for="" class="form-label">Serial de Temperatura</label>
-        <input id="serialTemperatura" name="serialTemperatura" type="text"  class="form-control" value="{{$kitSensor->serialTemperatura}}">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label">Fecha de Colocacion</label>
-        <input id="fechaColocacion" name="fechaColocacion" type="date"  class="form-control" value="{{$kitSensor->fechaColocacion}}">
-    </div>
-    
-    <!--botones-->
-    <a href="/kitSensores" class="btn btn-secondary" tabindex="8">Cancelar</a>
-    <button type="submit" class="btn btn-primary" tabindex="9">Guardar</button>
-</form>
 </div>
+<!-- /Card -->
+
 @endsection
